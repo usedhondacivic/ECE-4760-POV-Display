541d6dd3896af2cc4f7f08452f2275b7307baf69
Michael Crum <mmc323@cornell.edu>
2022-11-02 15:29:26 -0400

    Hello world

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/pwm.h"
+#include "pico/binary_info.h"
+#include "hardware/adc.h"
+#include "hardware/uart.h"
+
+int main()
+{
+    printf("Hello world");
+}
