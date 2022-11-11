e2bf042f353854cf0d64c5ac4cfb1546ccc19f6f
Joseph Adin Horwitz <jah569@cornell.edu>
2022-11-11 15:58:06 -0500

    wifi working

--- PICO/main.c
+++ PICO/main.c
@@ -7,12 +7,33 @@
 
 #define WS2812_PIN 2
 #define NUM_PIXELS 48
+#include "./utils/picow_tcp_client.h"
+
+#define WIFI_SSID "I14"
+#define WIFI_PASSWORD "horwitz3"
+#define TEST_TCP_SERVER_IP "172.20.10.2"
 
 int main()
 {
     stdio_init_all();
 
-    ws2812_init(NUM_PIXELS, WS2812_PIN, 0);
+    if (cyw43_arch_init()) {
+        printf("failed to initialise\n");
+        return 1;
+    }
+    cyw43_arch_enable_sta_mode();
 
-    ws2812_pattern_snakes(NUM_PIXELS, 0);
+    printf("Connecting to WiFi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("failed to connect.\n");
+        return 1;
+    } else {
+        printf("Connected.\n");
+    }
+    run_tcp_client_test();
+    cyw43_arch_deinit();
+
+    // ws2812_init(NUM_PIXELS, WS2812_PIN, 0);
+
+    // ws2812_pattern_snakes(NUM_PIXELS, 0);
 }
---
+++ PICO/utils/picow_tcp_client.h
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <string.h>
+#include <time.h>
+
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+#include "lwip/pbuf.h"
+#include "lwip/tcp.h"
+
+
+
+static void dump_bytes(const uint8_t *bptr, uint32_t len) ;
+
+// typedef struct TCP_CLIENT_T_ {
+//     struct tcp_pcb *tcp_pcb;
+//     ip_addr_t remote_addr;
+//     uint8_t buffer[BUF_SIZE];
+//     int buffer_len;
+//     int sent_len;
+//     bool complete;
+//     int run_count;
+//     bool connected;
+// } TCP_CLIENT_T;
+
+static err_t tcp_client_close(void *arg) ;
+
+// Called with results of operation
+static err_t tcp_result(void *arg, int status) ;
+
+static err_t tcp_client_sent(void *arg, struct tcp_pcb *tpcb, u16_t len) ;
+
+static err_t tcp_client_connected(void *arg, struct tcp_pcb *tpcb, err_t err) ;
+
+static err_t tcp_client_poll(void *arg, struct tcp_pcb *tpcb);
+
+static void tcp_client_err(void *arg, err_t err) ;
+
+err_t tcp_client_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err) ;
+
+static bool tcp_client_open(void *arg) ;
+
+// Perform initialisation
+// static TCP_CLIENT_T* tcp_client_init(void) ;
+
+void run_tcp_client_test(void) ;
