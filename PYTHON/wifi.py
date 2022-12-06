fff8b2444b696fda4df297dca349da1fe44504a3
Joey Horwitz <jah569@cornell.edu>
2022-12-06 11:47:33 -0500

    added photos and updated send options

--- PYTHON/wifi.py
+++ PYTHON/wifi.py
@@ -3,21 +3,29 @@ import socket
 import random
 import winwifi
 import bmp
+import argparse
+import numpy as np
+
 
 # These constants should match the client
 SERVER_PORT = 4242
 
-LED_NUM = 4
-ROTATIONS = 10
-RPB = 5
+# Import the library
+import argparse
+# Create the parser
+parser = argparse.ArgumentParser()
+# Add an argument
+parser.add_argument('--r', type=int, default=90)
+parser.add_argument('--debug', action='store_true')
+
+# Parse the argument
+args = parser.parse_args()
+
+LED_NUM = 40
+ROTATIONS = 90
+RPB = 2
 BUF_SIZE = LED_NUM * RPB * 3
-PACKET_NUM = ROTATIONS // RPB
-
-
-# Get image:
-path = "C:/Users/horwi/Downloads/dog.bmp"
-rot_arr, _, _ = bmp.bmp_to_rad(path, LED_NUM, ROTATIONS)
-
+PACKET_NUM = args.r // RPB
 
 # Start connection
 # wlan = network.WLAN(network.STA_IF)
@@ -29,7 +37,7 @@ try:
     interfaces = [i.ssid for i in winwifi.WinWiFi.get_connected_interfaces()]
 except:
     interfaces = []
-print(interfaces)
+print(f'Connected to {interfaces[0]}')
 
 
 # Wait for connect success or failure
@@ -49,46 +57,82 @@ while max_wait > 0:
 # Handle connection error
 if len(interfaces) == 0:
     raise RuntimeError('wifi connection failed')
-# else:
-#     print('connected')
-#     status = wlan.ifconfig()
-#     print( 'ip = ' + status[0] )
 
-# Open socket to the server
-sock = socket.socket()
-addr = ('0.0.0.0', SERVER_PORT)
-sock.bind(addr)
-sock.listen(1)
-print('server listening on', addr)
 
-# Wait for the client
-con = None
-con, addr = sock.accept()
-print('client connected from', addr)
 
-# repeat test for a number of iterations
-for i in range(PACKET_NUM):
 
-    # Generate a buffer of random data
-    # random_list = []
-    # for n in range(BUF_SIZE):
-    #     random_list.append(random.randint(0, 255))
-    # write_buf = bytearray(random_list)
+def send_data(name, debug=False):
+    # Get image:
+    if name == 'r':
+        shp = np.zeros((args.r, LED_NUM, 3))
+        rot_arr = np.full_like(shp, [200,1,1], dtype=int)
+    path = f"C:/Users/horwi/git/ECE-4760-final-project/PYTHON/pov_images/{name}.bmp"
+    rot_arr, _, _ = bmp.bmp_to_rad(path, LED_NUM, args.r)
+    # 
 
-    # Take RPB rotations from image
-    curr_data = rot_arr[5*i : 5*i+5][:][:]
-    print(curr_data)
-    write_buf = bytearray(curr_data.flatten())
+    # Open socket to the server
+    sock = socket.socket()
+    addr = ('0.0.0.0', SERVER_PORT)
+    sock.bind(addr)
+    sock.listen(1)
+    print('server listening on', addr)
 
-    # write BUF_SIZE bytes to the client
-    write_len = con.send(bytearray(write_buf))
-    print('Written %d bytes to client' % write_len)
+    # Wait for the client
+    con = None
+    con, addr = sock.accept()
+    print('client connected from', addr)
 
-    # Check size of data written
-    if write_len != BUF_SIZE:
-        raise RuntimeError('wrong amount of data written')
 
-# All done
-con.close()
-sock.close()
-print("test completed")
+    total_bytes = 0
+
+    # repeat test for a number of iterations
+    for i in range(PACKET_NUM):
+
+        # Take RPB rotations from image
+        curr_data = rot_arr[RPB*i : RPB*i+RPB][:][:]
+        write_buf = bytearray(curr_data.flatten())
+        if debug:
+            print(len(curr_data.flatten()))
+            print(len(write_buf))
+            prstr = ""
+            for i, b in enumerate(write_buf):
+                if i % 3 == 0: prstr += 'r: ' + str(int(b)) + ' '
+                if i % 3 == 1: prstr += 'g: ' + str(int(b)) + ' '
+                if i % 3 == 2: prstr += 'b: ' + str(int(b)) + '\n'
+            print(prstr)
+        # write BUF_SIZE bytes to the client
+        write_len = con.send(write_buf)
+        print('Wrote %d bytes to client' % write_len)
+        total_bytes += write_len
+
+        # Check size of data written
+        if write_len != BUF_SIZE:
+            raise RuntimeError('wrong amount of data written')
+
+    # All done
+    con.close()
+    sock.close()
+
+    print(f"In total wrote {total_bytes} bytes")
+    print("send completed")
+
+while True:
+    print('\nEnter an image name:')
+    name = input()
+    if name in ['e', 'exit']: break
+    if name == 'flower':
+        for _ in range(7):
+            print('Sending animation')
+            for i in range(4):
+                try: 
+                    send_data(f"flower/{i}")
+                except Exception as e:
+                    print(f'Send failed on frame {i} with below message:')
+                    print(e)
+    print('Sending image')
+    try: 
+        send_data(name, args.debug)
+    except Exception as e:
+        print('Send failed with below message:')
+        print(e)
+
